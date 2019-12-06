0adb7fd492450b0c1f67307d2e79c8ea7b286bb7
Mega Silvia <megasilvia5555@gmail.com>
2019-12-06 13:34:27 +0700

    25
    25

--- js/main.js
+++ js/main.js
@@ -309,3 +309,12 @@
 
 
 }());
+
+
+$(window).scroll(function() {
+	if ($(document).scrollTop() > 50) {
+	  $('nav').addClass('shrink');
+	} else {
+	  $('nav').removeClass('shrink');
+	}
+  });
--- produk_fashion.php
+++ produk_fashion.php
@@ -65,24 +65,14 @@
 				<div class="container">
 					<div class="row">
 						<div class="col-xs-2">
-							<div id="colorlib-logo"><a href="index.html">ABAGBAG</a></div>
+							<div id="colorlib-logo"><a href="index.php">ABAGBAG</a></div>
 						</div>
 						<div class="col-xs-10 text-right menu-1">
 							<ul>
-								<li class="active"><a href="index.html">Beranda</a></li>
-								<li class="has-dropdown">
-									<a href="produk.html">Produk</a>
-									<ul class="dropdown">
-										<li><a href="product-detail.html">Product Detail</a></li>
-										<li><a href="keranjang.html">Shipping Cart</a></li>
-										<li><a href="checkout.html">Checkout</a></li>
-										<li><a href="order-complete.html">Order Complete</a></li>
-										<li><a href="add-to-wishlist.html">Wishlist</a></li>
-									</ul>
-								</li>
-								<li><a href="pembayaran.html">Pembayaran</a></li>
-								<li><a href="contact.html">Hubungi Kami</a></li>
-								<li><a href="keranjang.html"><i class="icon-shopping-cart"></i> Keranjang</a></li>
+								<li class="active"><a href="index.php">Beranda</a></li>
+								<li><a href="pembayaran.php">Pembayaran</a></li>
+								<li><a href="contact.php">Hubungi Kami</a></li>
+								<li><a href="keranjang.php"><i class="icon-shopping-cart"></i> Keranjang</a></li>
 							</ul>
 						</div>
 					</div>
